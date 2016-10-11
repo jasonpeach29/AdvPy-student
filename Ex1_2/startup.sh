033b616a08277373f9c7a440306f2aa8f551e22d
Jason Enochs <jason@codehugger.com>
2016-10-10 23:16:01 -0400

    Update Day1
        Updated all day 1 content

---
+++ Ex1_2/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###############################################################################
+# FILE NAME : startup.sh
+# AUTHOR : J.Enochs
+# CREATION DATE : 20-Aug-2016
+# LAST MODIFIED : 20-Aug-2016
+# DESCRIPTION :
+#
+###############################################################################/
+gnome-terminal --full-screen -e ./quiz_initial.py 
