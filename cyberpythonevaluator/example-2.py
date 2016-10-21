630032c9d97f5bba34ede574767e0c6d76733883
Jason Peach <jasonpeach29@gmail.com>
2016-10-21 09:05:11 -0400

    20 oct

--- cyberpythonevaluator/example-2.py
+++ cyberpythonevaluator/example-2.py
@@ -8,8 +8,14 @@ print "Description", challenge.description
 print "Example Input", challenge.example_input
 print "Example Output", challenge.example_output
 
-def solution (lhs, rhs) :
-    return lhs + rhs
+def solution (number, group) :
+    group = '10201'
+    if number in group:
+        number % 2 == 0
+        print true
+    else:
+        print false
+
 
 #print challenge.validate(solution, debug=True)
 print challenge.validate(solution, debug=True)
